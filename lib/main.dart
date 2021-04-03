b1c666ec34e4b4dd674ff2efbc3508a80cf4f894
Your Name <manaskamal24@gmail.com>
2021-04-03 17:22:17 +0530

    My first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+//import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+void main()
+{
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget
+{
+
+   Widget build(BuildContext arg)
+    {
+      return MaterialApp(home:Text("Hi, how are you ?"));
+    }
+    ,./
+}
+
